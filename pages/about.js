29a8843363ff37c17c7f9afe34b2ae2e4bb98bbc
Xelem <anselmchizurum@gmail.com>
2022-06-16 18:07:46 +0100

    Added the logo

--- pages/about.js
+++ pages/about.js
@@ -1,20 +1,28 @@
+import Head from "next/head";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-        incidunt id, perferendis consectetur repellendus, ratione sequi minima
-        ut ipsum facere repellat et illum facilis, ex obcaecati fuga quibusdam
-        est libero?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
-        incidunt id, perferendis consectetur repellendus, ratione sequi minima
-        ut ipsum facere repellat et illum facilis, ex obcaecati fuga quibusdam
-        est libero?
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+          incidunt id, perferendis consectetur repellendus, ratione sequi minima
+          ut ipsum facere repellat et illum facilis, ex obcaecati fuga quibusdam
+          est libero?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+          incidunt id, perferendis consectetur repellendus, ratione sequi minima
+          ut ipsum facere repellat et illum facilis, ex obcaecati fuga quibusdam
+          est libero?
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -7,23 +7,29 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h2 className={styles.title}>Homepage</h2>
-      <p className={styles.text}>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi,
-        fuga? Perspiciatis, fuga architecto voluptatibus asperiores sequi
-        aspernatur expedita nihil illum inventore dolor itaque quidem quam!
-        Nobis nesciunt sequi accusantium odit.
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi,
-        fuga? Perspiciatis, fuga architecto voluptatibus asperiores sequi
-        aspernatur expedita nihil illum inventore dolor itaque quidem quam!
-        Nobis nesciunt sequi accusantium odit.
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h2 className={styles.title}>Homepage</h2>
+        <p className={styles.text}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi,
+          fuga? Perspiciatis, fuga architecto voluptatibus asperiores sequi
+          aspernatur expedita nihil illum inventore dolor itaque quidem quam!
+          Nobis nesciunt sequi accusantium odit.
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi,
+          fuga? Perspiciatis, fuga architecto voluptatibus asperiores sequi
+          aspernatur expedita nihil illum inventore dolor itaque quidem quam!
+          Nobis nesciunt sequi accusantium odit.
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
