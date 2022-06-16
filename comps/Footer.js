52fabd3b0f68700aaa68e8ca8c7e78eed997f52a
Xelem <anselmchizurum@gmail.com>
2022-06-16 15:57:04 +0100

    Completed routing and learned how to inject css

---
+++ comps/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <footer>Copyright Anselm 2022</footer>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+        incidunt id, perferendis consectetur repellendus, ratione sequi minima
+        ut ipsum facere repellat et illum facilis, ex obcaecati fuga quibusdam
+        est libero?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+        incidunt id, perferendis consectetur repellendus, ratione sequi minima
+        ut ipsum facere repellat et illum facilis, ex obcaecati fuga quibusdam
+        est libero?
+      </p>
+    </div>
+  );
+};
+
+export default About;
